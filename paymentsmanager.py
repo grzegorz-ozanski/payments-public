d8c12445a6f97299202944023d8d9a98180a2ee2
Grzegorz Ożański <greggor@gmail.com>
2025-04-11 15:44:24 +0200

    Quit browser after collecting all payments

--- paymentsmanager.py
+++ paymentsmanager.py
@@ -15,17 +15,26 @@ class PaymentsManager:
     debug_mode: bool
     payments: List[Payment] = field(default_factory=list)
 
+    def _get_payments_for_service(self, service: BaseService):
+        payments = []
+        try:
+            print("Processing service %s..." % service.name)
+            service.login(self.browser)
+            payments = sorted(service.get_payments(),
+                              key=lambda value: self.accounts.sort_key(value.account))
+        except Exception as e:
+            print(e)
+            print("Cannot get payments for service %s!" % service.name)
+        finally:
+            service.logout()
+        return payments
+
     def collect(self):
-        for service in self.services:
-            try:
-                print("Processing service %s..." % service.name)
-                service.login(self.browser)
-                self.payments += sorted(service.get_payments(),
-                                        key=lambda value: self.accounts.sort_key(value.account))
-                service.logout()
-            except Exception as e:
-                print(e)
-                print("Cannot get payments for service %s!" % service.name)
+        try:
+            for service in self.services:
+                self.payments += self._get_payments_for_service(service)
+        finally:
+            self.browser.quit()
 
     def print(self):
         for payment in self.payments:
