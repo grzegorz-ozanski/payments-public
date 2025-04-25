cc2bf7e3bbd24b11a10a3690583926eaafdeb0dd
Grzegorz Ożański <greggor@gmail.com>
2025-04-25 15:10:47 +0200

    Make mask-env-secrets.ps1 compatible with being called with "pwsh -File"

--- .github/scripts/mask-env-secrets.ps1
+++ .github/scripts/mask-env-secrets.ps1
@@ -1,10 +1,12 @@
 param (
   [Parameter(Position = 0, Mandatory = $true)]
-  [string[]]$Patterns
+  [string]$Sufficies
 )
 
+$SufficiesList = $Sufficies -split ","
+
 foreach ($key in [System.Environment]::GetEnvironmentVariables().Keys) {
-  foreach ($suffix in $Patterns) {
+  foreach ($suffix in $SufficiesList) {
     if ($key.EndsWith($suffix)) {
       $val = [System.Environment]::GetEnvironmentVariable($key)
       if ($val) {
