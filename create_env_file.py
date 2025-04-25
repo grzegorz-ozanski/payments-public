548a07e6e1388f7619ea6a4a344257ee31b9637e
Grzegorz Ożański <greggor@gmail.com>
2025-04-25 12:53:52 +0200

    Helper for creating .env files

---
+++ create_env_file.py
@@ -0,0 +1,35 @@
+"""
+    Dumps credentials into .env file
+"""
+
+import argparse
+
+import keyring
+
+import providers
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse program arguments
+    :return: parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Prepare an .env file to be used e.g. in GitHub Secrets')
+    parser.add_argument('-o', '--output', default='.env', help='output env file')
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    """
+    Main function
+    """
+    args = parse_args()
+    with open(args.output, "w") as file:
+        for provider in [provider.lower() for provider in providers.__all__]:
+            for cred in ('username', 'password'):
+                print(f'{provider.upper()}_{cred.upper()}={keyring.get_password(provider, cred)}', file=file)
+
+
+if __name__ == '__main__':
+    main()
