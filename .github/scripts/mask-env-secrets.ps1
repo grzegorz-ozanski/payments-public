b07517b886c083964dbeb56ef74e6ce368424aaf
Grzegorz Ożański <greggor@gmail.com>
2025-04-28 17:08:14 +0200

    Allow mask variables source other than environment

--- .github/scripts/mask-env-secrets.ps1
+++ .github/scripts/mask-env-secrets.ps1
@@ -1,14 +1,35 @@
 param (
   [Parameter(Position = 0, Mandatory = $true)]
-  [string]$Sufficies
+  [string]$Sufficies,
+  [Parameter(Position = 1)]
+  [string]$EnvVariables = $null
 )
+
+function Get-ValuesToMask {
+  param (
+    [Parameter(Position=0)]
+    [string]$EnvVariablesString
+  )
+  if ($EnvVariablesString) {
+    $dict = @{}
+    foreach ($line in $EnvVariablesString.Trim().Split("`n")) {
+        $key, $value = $line.Split("=")
+        $dict[$key] = $value
+    }
+    $dict
+  } else {
+    [System.Environment]::GetEnvironmentVariables()
+  }
+}
+
+$dict = Get-ValuesToMask $EnvVariables
 
 $SufficiesList = $Sufficies -split ","
 
-foreach ($key in [System.Environment]::GetEnvironmentVariables().Keys) {
+foreach ($key in $dict.Keys) {
   foreach ($suffix in $SufficiesList) {
     if ($key.EndsWith($suffix)) {
-      $val = [System.Environment]::GetEnvironmentVariable($key)
+      $val = $dict[$key]
       if ($val) {
         Write-Output "::add-mask::$val"
         Write-Output "Masked $key"
