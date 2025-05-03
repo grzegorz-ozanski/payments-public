7542f7fee4a9d531af0b2d20aa706f98e3cd3957
Grzegorz Ożański <greggor@gmail.com>
2025-05-03 21:33:05 +0200

    Re-read entire invoices list on StaleElementReferenceException

--- providers/pgnig.py
+++ providers/pgnig.py
@@ -26,20 +26,23 @@ class Pgnig(BaseService):
         log.info("Opening invoices menu...")
         self.save_trace_logs("pre-invoices-click")
         invoices_menu.click()
+        log.debug("Waiting for page load completed...")
         self.browser.wait_for_page_inactive()
-        log.info("Getting invoices list...")
-        invoices = self.browser.wait_for_elements(By.CLASS_NAME, "main-row-container")
-        log.info("Filtering unpaid invoices...")
-        unpaid_invoices = []
-        for index, item in enumerate(invoices):
+        unpaid_invoices = None
+        for i in range(10):
+            index = 0
+            item = None
             try:
-                if item.find_element(By.CLASS_NAME, 'button').text == "Zapłać":
-                    unpaid_invoices.append(item)
+                log.info("Getting filtered invoices list...")
+                unpaid_invoices = []
+                for index, item in enumerate(self.browser.wait_for_elements(By.CLASS_NAME, "main-row-container")):
+                    if item.find_element(By.CLASS_NAME, 'button').text == "Zapłać":
+                        unpaid_invoices.append(item)
+                break
             except StaleElementReferenceException:
                 log.warning(f"Stale element encountered during filtering invoices.\n"
                             f"Element index: {index}\n"
                             f"Element details:\n{self.browser.dump_element(item)}")
-                continue
         log.debug("Creating payments dict...")
         payments_dict = {}
         for invoice in unpaid_invoices:
