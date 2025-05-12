9a63d018c2dbf3adf4e14b81bca17e77e6ead50d
Grzegorz Ożański <greggor@gmail.com>
2025-05-12 15:58:11 +0200

    Add type hints and documentation

--- payments/payment.py
+++ payments/payment.py
@@ -1,3 +1,8 @@
+"""
+Payments module
+"""
+from typing import TextIO
+
 from locations import Location
 from browser import setup_logging
 
@@ -6,19 +11,41 @@ from parsers import *
 log = setup_logging(__name__)
 
 class Payment:
-    def __init__(self, amount=0, due_date=None, location: Location | None = None, provider: str = ''):
+    """
+    Single payment
+    """
+    def __init__(self,
+                 amount: int = 0,
+                 due_date: object = None,
+                 location: Location | None = None,
+                 provider: str = '') -> None:
+        """
+        Payment constructor
+
+        :param amount: payment amount
+        :param due_date: payment due date
+        :param location: payment location
+        :param provider: payment provider
+        """
         self.amount = parse_amount(amount)
         self.due_date = parse_date(due_date)
         self.location = location
         self.provider = provider
 
-    def __str__(self):
-        return f"{self.location.name} {self.due_date} {self.amount}"
+    def __str__(self) -> str:
+        return f'{self.location.name} {self.due_date} {self.amount}'
 
-    def is_empty(self):
-        # log.debug("amount: %s, due_date: %s, acc")
+    def is_empty(self) -> bool:
+        """
+        Check if object is empty
+        :return: 'True' if object is empty, 'False' otherwise
+        """
         return self.amount is None or self.due_date is None or self.location is None
 
-    def print(self, stream=None):
-        print(f"{self.provider + ' ' if self.provider else ''}"
-              f"{self.amount} {self.location.name} {self.due_date}", file=stream)
+    def print(self, stream: TextIO = None) -> None:
+        """
+        Print the object
+        :param stream: Stream to print to ('None' to pring to stdout)
+        """
+        print(f'{self.provider + " " if self.provider else ""}'
+              f'{self.amount} {self.location.name} {self.due_date}', file=stream)
--- payments/paymentsmanager.py
+++ payments/paymentsmanager.py
@@ -1,3 +1,6 @@
+"""
+Payments manager module
+"""
 from dataclasses import dataclass, field
 
 from browser import Browser
@@ -7,41 +10,57 @@ from providers.provider import Provider
 
 @dataclass
 class PaymentsManager:
+    """
+    Payments manager
+    """
     browser: Browser
     services: list[Provider] | Provider
     debug_mode: bool
     payments: list[Payment] = field(default_factory=list)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """
+        If single item is provided, change it into one-element list
+        """
         if not isinstance(self.services, list):
             self.services = [self.services]
 
-    def _get_payments_for_service(self, service: Provider):
+    def _get_payments_for_service(self, service: Provider) -> list[Payment]:
         payments = []
         try:
-            print("Processing service %s..." % service.name)
+            print(f'Processing service {service.name}...')
             service.login(self.browser)
             payments = sorted(service.get_payments(), key=lambda value: value.location.key)
         except Exception as e:
-            print(f"{e.__class__.__name__}:{str(e)}")
-            print("Cannot get payments for service %s!" % service.name)
+            print(f'{e.__class__.__name__}:{str(e)}\n'
+                  f'Cannot get payments for service {service.name}!')
             service.save_error_logs()
         finally:
             service.logout()
         return payments
 
-    def collect(self):
+    def collect(self) -> None:
+        """
+        Collect payments for all providers
+        """
         try:
             for service in self.services:
                 self.payments += self._get_payments_for_service(service)
         finally:
             self.browser.quit()
 
-    def print(self):
+    def print(self) -> None:
+        """
+        Print payments for all providers
+        """
         for payment in self.payments:
             payment.print()
 
-    def write(self, filename):
+    def write(self, filename: str) -> None:
+        """
+        Write payments to file
+        :param filename: file name
+        """
         with open(filename, 'w') as stream:
             for payment in self.payments:
                 payment.print(stream)
