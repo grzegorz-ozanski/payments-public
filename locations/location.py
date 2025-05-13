d20be033840d5d689f62515c9bfc233c64b7c3c7
Grzegorz Ożański <greggor@gmail.com>
2025-05-13 13:08:15 +0200

    Location to string convertion

--- locations/location.py
+++ locations/location.py
@@ -1,7 +1,10 @@
 class Location:
     next_key: int = 0
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
         self.key = Location.next_key
         Location.next_key += 1
+
+    def __str__(self) -> str:
+        return self.name
