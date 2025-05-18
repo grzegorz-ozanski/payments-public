f47263f47a4d6149d4be682bc969e43f46f33d72
Grzegorz Ożański <greggor@gmail.com>
2025-05-18 10:05:54 +0200

    Use more legible condition

--- .github/scripts/compare-status.ps1
+++ .github/scripts/compare-status.ps1
@@ -96,7 +96,7 @@ if ($ScriptOutput -and (Test-Path $ScriptOutput)) {
   Append-IfExists "- 📃**Script Output**:" -Path $GitHubSummary
   Get-Content -Path $ScriptOutput | Append-IfExists -Path $GitHubSummary
 }
-if ($DiffFile -and (Test-Path $DiffFile) -and ($current -eq "failure")) {
+if ($DiffFile -and (Test-Path $DiffFile) -and ($CompareStatus -eq "changed")) {
   Append-IfExists "- 🟥🟩**Diff**:" -Path $GitHubSummary
   Get-Content -Path $DiffFile | Append-IfExists -Path $GitHubSummary
 }
