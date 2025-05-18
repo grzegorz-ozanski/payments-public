838297b86ca876d9d9667922942bde34f209936a
Grzegorz Ożański <greggor@gmail.com>
2025-05-18 12:12:50 +0200

    Debug actual run

--- .github/scripts/compare-status.ps1
+++ .github/scripts/compare-status.ps1
@@ -96,7 +96,7 @@ if ($ScriptOutput -and (Test-Path $ScriptOutput)) {
   Append-IfExists "- 📃**Script Output**:" -Path $GitHubSummary
   Get-Content -Path $ScriptOutput | Append-IfExists -Path $GitHubSummary
 }
-if ($DiffFile -and (Test-Path $DiffFile) -and ($CompareStatus -eq "unchanged")) {
+if ($DiffFile -and (Test-Path $DiffFile) -and ($CompareStatus -eq "changed")) {
   Append-IfExists "- 🟥🟩**Diff**:" -Path $GitHubSummary
   Get-Content -Path $DiffFile | Append-IfExists -Path $GitHubSummary
 }
