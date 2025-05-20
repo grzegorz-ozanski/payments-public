489046c225c6e7a555994e924ee41f8622dbed7f
Grzegorz Ożański <greggor@gmail.com>
2025-05-20 14:28:31 +0200

    Some optimizations in Multimedia provider login and payments processing

--- providers/multimedia.py
+++ providers/multimedia.py
@@ -2,7 +2,6 @@ from datetime import date
 from time import sleep
 from typing import cast
 
-from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 
 from browser import setup_logging
@@ -28,50 +27,44 @@ class Multimedia(Provider):
         super().__init__(url, keystore_service, locations, user_input, password_input, pre_login_delay=5)
 
     def _get_location(self, amount: str):
-        location = [location for value, location in self._locations_map.items() if amount.startswith(value)]
-        if not location:
+        try:
+            return next(location for value, location in self._locations_map.items() if amount.startswith(value))
+        except StopIteration:
             raise Exception(f"Cannot find suitable location for payment '{amount}'!")
-        return location[0]
 
     def login(self, browser, load=True):
-        wait = 10
+        wait = 5
         num_retries = 10
         for i in range(num_retries):
             log.debug(f'Login attempt {i + 1}')
             try:
-                super().login(browser, load)
+                super().login(browser, load if i == 0 else False)
             except Exception as ex:
                 if i == num_retries - 1:
                     raise ex
-            self._browser.wait_for_page_inactive()
-            if self._browser.wait_for_element(By.CSS_SELECTOR, 'span.logonFailureText'):
+            self._browser.wait_for_page_inactive(2)
+            if self._browser.wait_for_element(By.CSS_SELECTOR, 'span.logonFailureText', 2):
                 log.debug(f'Login failed, retrying after {wait} seconds')
                 self.save_trace_logs(f'failed-login-attempt-{i}')
             else:
-                self.logged_in = True
                 return
+        reason = "unknown"
+        if self._browser.find_elements(By.ID, "formCaptcha"):
+            reason = "CAPTCHA required"
+        raise RuntimeError(f"Couldn't log in in {num_retries} attempts! Reason: {reason}")
 
     def _read_payments(self):
         log.info("Getting payments...")
-        payments = []
-        if not self.logged_in:
-            raise RuntimeError("Not logged in!")
-        while any(item.is_empty() for item in payments) or len(payments) == 0:
-            sleep(0.1)
-            payments = []
-            invoices = self._browser.wait_for_elements(By.CLASS_NAME, "invoiceInfo")
-            if invoices is None:
-                for location in self.locations:
-                    payments.append(Payment(due_date=self.default_due_date, location=location, provider=self.name))
-                return payments
-            for invoice in invoices:
-                try:
-                    amount = invoice.find_element(By.CLASS_NAME, "kwota").text
-                    due_date = invoice.find_element(By.CLASS_NAME, "platnoscDo").text
-                    log.debug("Got amount '%s'" % amount)
-                    location = self._get_location(amount)
-                    payments.append(Payment(amount, due_date, location, self.name))
-                except StaleElementReferenceException:
-                    log.debug("StaleElementReferenceException occurred, retrying")
-                    payments.append(Payment())
+        sleep(0.1)
+        payments = [Payment(due_date=self.default_due_date, location=location, provider=self.name) for location in self.locations]
+        invoices = self._browser.wait_for_elements(By.CLASS_NAME, "invoiceInfo")
+        if invoices is None:
+            return payments
+        for invoice in invoices:
+            amount = invoice.find_element(By.CLASS_NAME, "kwota").text
+            due_date = invoice.find_element(By.CLASS_NAME, "platnoscDo").text
+            log.debug("Got amount '%s'" % amount)
+            location = self._get_location(amount)
+            index = payments.index(next(payment for payment in payments if payment.location == location))
+            payments[index] = Payment(amount, due_date, location, self.name)
         return payments
