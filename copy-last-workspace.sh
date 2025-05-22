34a06e6504fb2f7cba0e9b1b72d047cc01e64ebd
Grzegorz Ożański <greggor@gmail.com>
2025-05-22 15:48:00 +0200

    Script for copying last workspace from Linux Docker compose runner

---
+++ copy-last-workspace.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ARTIFACTS_HOST_NAME=blackview.perch-antares.ts.net
+ARTIFACTS_HOST_USER=grzegorz
+LAST_WORKSPACE_ROOT=/tmp
+LAST_WORKSPACE=last_workspace
+TARGET_ROOT="run"
+SSH_HOST=${ARTIFACTS_HOST_USER}@${ARTIFACTS_HOST_NAME}
+SRC="${LAST_WORKSPACE_ROOT}/${LAST_WORKSPACE}"
+DST="${TARGET_ROOT}/${LAST_WORKSPACE}"
+PACKER="zstd -T0 --no-progress -o"
+ARCHIVE_NAME="${LAST_WORKSPACE}.tar.zst"
+RUNNER_NAME=github-runner
+RUNNER_ROOT=/tmp/actions-runner/jobs/
+
+echo "Copyinng ${JOB_NAME} run #${RUN_NUMBER} artifacts from ${SRC} to ${DST}"
+ssh -t "${SSH_HOST}" "
+    rm -rf ${SRC} && 
+    docker cp ${RUNNER_NAME}:${RUNNER_ROOT}/${LAST_WORKSPACE} ${LAST_WORKSPACE_ROOT} && 
+    cd ${LAST_WORKSPACE_ROOT} && 
+    rm -f ${ARCHIVE_NAME} && 
+    tar -c ${LAST_WORKSPACE} | pv -s \$(du -sb ${LAST_WORKSPACE} | awk '{print \$1}') | ${PACKER} ${ARCHIVE_NAME}
+"
+scp -r "${SSH_HOST}:${LAST_WORKSPACE_ROOT}/${ARCHIVE_NAME}" "${TARGET_ROOT}/${ARCHIVE_NAME}"
+bash -c "cd ${TARGET_ROOT} && tar -xf ${ARCHIVE_NAME} && rm -f ${ARCHIVE_NAME}"
