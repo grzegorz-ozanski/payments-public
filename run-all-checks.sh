fa564a58a71e08d048bead847063a04341146b2d
Grzegorz Ożański <greggor@gmail.com>
2025-06-02 08:27:38 +0200

    Add script for running all checks, cleanup

--- run-all-checks.sh
+++
@@ -1,4 +0,0 @@
-pytest --cov=payments --cov=providers --cov=lookuplist --cov-report=term --cov-report=html
-mypy . --strict --ignore-missing-imports --exclude run
-ruff check .
-
--- run-checks.sh
+++ run-checks.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 pytest --cov=payments --cov=providers --cov=lookuplist --cov-report=term --cov-report=html
 mypy . --strict --ignore-missing-imports --exclude run
 ruff check .
-
+rm -rf error
