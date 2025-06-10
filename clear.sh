fb01bfd0851f01e57d4c8d6b8f88d002c042a51c
Grzegorz Ożański <greggor@gmail.com>
2025-06-10 15:23:02 +0200

    Prevent globbing expansion

--- clear.sh
+++ clear.sh
@@ -7,9 +7,9 @@ if [[ "${1:-}" == "all" ]]; then
   FILES+=(run/*)
 fi
 
-# Użyj globbing protection: tylko jeśli istnieją pliki
+# User globbing protection: run only if files exist
 for f in "${FILES[@]}"; do
   if compgen -G "$f" > /dev/null; then
-    rm -rf $f
+    rm -rf "$f"
   fi
 done
