f27104d1b481f5a10cb477a8640e196d8497c818
Grzegorz Ożański <greggor@gmail.com>
2026-02-04 08:15:18 +0100

    End each message with new line when in CI environment

--- payments/console/print_progress.py
+++ payments/console/print_progress.py
@@ -1,9 +1,10 @@
 """
     Functions for printing a provider operation progress
 """
+import os
 from functools import partial
 
-print_progress = partial(print, end='', flush=True)
+print_progress = partial(print, end='\n...' if "GITHUB_ACTIONS" in os.environ else '', flush=True)
 
 print_done = partial(print, flush=True)
 
@@ -15,6 +16,11 @@ def print_stage(msg: str, stage_id: int, number_of_stages: int) -> None:
     :param stage_id: stage id
     :param number_of_stages: total number of stages
     """
+    # Or, if GHA starts to have problems with live output:
+    # message = f'...{msg} {stage_id + 1} of {number_of_stages}...'
+    # ...
+    # print_done("")
+    # print_progress(message)
     message = f'\n...{msg} {stage_id + 1} of {number_of_stages}...'
     if stage_id + 1 == number_of_stages:
         message = f'{message}\n...'
